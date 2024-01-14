d3063d8903ddae71ca039c8807da1ff21902143f
unknown <adnanhamid29@gmail.com>
2024-01-14 20:21:49 +0530

    Palindrome with user input

--- Palindrome.js
+++ Palindrome.js
@@ -1,15 +1,22 @@
 function isPalindrome(s){
-    let last= s.length-1;
-    for (let i=0; i<Math.ceil(s.length/2);i++){
-        if(s[i]!=s[last]){
+   
+    let last=s.length -1;
+    for (let i=0;i< Math.ceil(s.length/2);i++){
+        if (s[i] !==s[last]){
             return false;
-
         }
-        last--;
-        
-       
+        last--
+
     }
     return true;
 
 }
-console.log(isPalindrome("xammax"))
+
+
+
+while(1){
+    let inputString= prompt("enter the string \n") 
+    console.log(isPalindrome(inputString))
+  
+    
+}
